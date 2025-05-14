13231c46da7b42ba9574f29fea1a7d0a4f49ef09
Isaac Li <ISL74@pitt.edu>
2025-05-14 20:01:54 +0000

    peano induction test

--- Analysis_Tao/AnalysisTao/Chapter2_NaturalNumbers/b_Addition.lean
+++ Analysis_Tao/AnalysisTao/Chapter2_NaturalNumbers/b_Addition.lean
@@ -1,5 +1,6 @@
 prelude
 import Init
+import Std.Classes
 import AnalysisTao.Chapter2_NaturalNumbers.a_PeanoAxioms
 
 namespace AnalysisTao.Chapter2_NaturalNumbers
@@ -7,18 +8,33 @@ namespace AnalysisTao.Chapter2_NaturalNumbers
 open PeanoNat
 
 --Definition 2.2.1 (Addition of natural numbers).
+@[simp]
 def add : PeanoNat → PeanoNat → PeanoNat
 | zero,    m => m
 | succ n,  m => PeanoNat.succ (add n m)
 
---Lemma 2.2.2: For any natural number n, n + 0 = n.
+-- Lemma 2.2.2
+theorem add_zero (n : PeanoNat) : add n 0 = n := by
+  induction n using PeanoNat.induction
+  case hbase => rfl
+  case hind n hn=>
+    dsimp
+    rw[hn]
 
---Lemma 2.2.3: For any natural numbers n and m, n+(m++) = (n + m)++.
+-- Lemma 2.2.3
+theorem add_succ (n m : PeanoNat) : add n (succ m) = succ (add n m) := by
+  sorry
 
---Proposition 2.2.4 (Addition is commutative).
+-- Proposition 2.2.4 (Addition is commutative).
+theorem add_comm (n m : PeanoNat) : add n m = add m n := by
+  sorry
 
---Proposition 2.2.5 (Addition is associative).
+-- Proposition 2.2.5 (Addition is associative).
+theorem add_assoc (a b c : PeanoNat) : add (add a b) c = add a (add b c) := by
+  sorry
 
---Proposition 2.2.6 (Cancellation law).
+-- Proposition 2.2.6 (Cancellation law).
+theorem add_left_cancel {a b c : PeanoNat} (h : add a b = add a c) : b = c := by
+  sorry
 
 end AnalysisTao.Chapter2_NaturalNumbers
